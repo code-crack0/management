c179fdf5196a08a17c812700c96de837fe269ac3
Samad Sayed <samaddxb004@gmail.com>
2024-11-13 10:56:26 +0400

    added env

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental:{
+    // middleware:true
+  }
+  
 };
 
 export default nextConfig;
